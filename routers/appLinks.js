bdf6e7abc7c3c8044a422c0118dfbff99082c6fd
ShreyasDhale <shreyasdhale7249@gmail.com>
2024-07-03 00:48:08 +0530

    get apks request

--- routers/appLinks.js
+++ routers/appLinks.js
@@ -6,33 +6,10 @@ const multer = require('multer');
 const path = require('path');
 
 const router = express.Router();
-
-const upload = multer({
-    limits: {
-        fileSize: 100000000 // Limit file size to 100MB
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.apk$/)) {
-            return cb(new Error('Only .apk files are allowed'));
-        }
-        cb(null, true);
-    }
-});
-
-// upload file local path and version is required for this request
-router.post('/uploads/apks/:version', upload.single('apk'), async (req, res) => {
+router.get('/apks', async (req, res) => {
     try {
-        const version = req.params.version;
-
-        // Check if file is uploaded
-        if (!req.file) {
-            throw new Error('File upload failed');
-        }
-
-        const filePath = req.file.path;
-        console.log('File uploaded to:', filePath);
-
-        const result = await AppInfo.uploadApk(version, filePath);
+        const result = await AppInfo.find().sort({ createdAt: -1 });
+        if (!result) throw new Error("No Release Yet!!")
         res.status(200).send(result);
     } catch (e) {
         res.status(400).send({ error: e.message });
