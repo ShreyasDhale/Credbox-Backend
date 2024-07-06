a7f8b53f27016f43a71edaab1cb168c5b06fc7c4
ShreyasDhale <shreyasdhale7249@gmail.com>
2024-07-06 12:54:01 +0530

    commit

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -30,7 +30,7 @@ router.post('/users/login', async (req, res) => {
 router.post('/users/vaildate', async (req, res) => {
     try {
         const user = await User.findByCredential(req.body.email, req.body.password);
-        res.status(200).send(user);
+        res.status(200).send({ user });
     } catch (e) {
         res.status(400).send(e.message);
         console.log(e);
